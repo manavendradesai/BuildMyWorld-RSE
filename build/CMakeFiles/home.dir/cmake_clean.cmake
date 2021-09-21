9b831bbc88a8c273813451c715a12a69599a6c20
Manavendra Desai <90561709+manavendradesai@users.noreply.github.com>
2021-09-20 22:58:40 -0400

    Add files via upload

---
+++ build/CMakeFiles/home.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/home.dir/script/welcome.cpp.o"
+  "libhome.pdb"
+  "libhome.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/home.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
